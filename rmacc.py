de43442a7422c2c934080671eb1cac44046f6f22
Max <maxigor.ferreira@gmail.com>
2023-01-19 17:31:41 -0300

    bump to 0.0.3-dev:wq

--- rmacc.py
+++ rmacc.py
@@ -3,34 +3,24 @@
 import subprocess
 import argparse
 import re
-
+import random
 
 def get_arguments():
-    parser = argparse.ArgumentParser(
-        prog = "rmacc",
-        description = "Change to a random MAC address"
-    )
-
-    parser.add_argument(
-        '-i', 
-        '--interface', 
-        dest='interface' , 
-        help='Interface to change its MAC address')
-
-    parser.add_argument(
-        '-m', 
-        '--mac', 
-        dest='new_mac',
-        help='New MAC address')
+    parser = argparse.ArgumentParser(prog = "rmacc", description = "Change to a random MAC address")
+    parser.add_argument('-i','--interface', dest='interface', help='Interface to change its MAC address')
+    parser.add_argument('-r','--random', help='Generate random MAC Address', action="store_true")
+    parser.add_argument('-m','--mac', dest='new_mac',help='User set the MAC Address')
     
     args = parser.parse_args()
+
     if not args.interface:
         parser.error("[-] Please specify an interface, use --help for more info")
-    if not args.new_mac:
-        parser.error("[-] Please specify a new mac, use --help for more info")
     
     return parser.parse_args()
-
+       
+def random_mac_address():
+    mac = [random.randint(0x00, 0xff) for _ in range(6)]
+    return ':'.join(map(lambda x: "%02x" % x, mac))
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
@@ -55,7 +45,17 @@ def get_current_mac(interface):
 
 
 args = get_arguments()
-change_mac(args.interface , args.new_mac)
 
 current_mac = get_current_mac(args.interface)
-print("The current MAC is: " + str(current_mac))
+print("[+] The current MAC is: " + str(current_mac))
+
+if args.random:
+    new_mac = random_mac_address()
+    change_mac(args.interface , new_mac)
+else:
+    change_mac(args.interface , args.new_mac)
+
+if current_mac == get_current_mac(args.interface):
+    print("[+] The new mac is: " + current_mac)
+else:
+    print("[-] Mac adress did not get changed T_T ")

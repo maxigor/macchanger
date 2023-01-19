5933439595b309880e52c1649330a699f1e7b276
Max <maxigor.ferreira@gmail.com>
2023-01-19 17:41:48 -0300

    refactored version

--- rmacc.py
+++ rmacc.py
@@ -5,16 +5,11 @@ import argparse
 import re
 import random
 
-def get_arguments():
+def parse_args():
     parser = argparse.ArgumentParser(prog = "rmacc", description = "Change to a random MAC address")
-    parser.add_argument('-i','--interface', dest='interface', help='Interface to change its MAC address')
+    parser.add_argument('-i','--interface', dest='interface', help='Interface to change its MAC address', required=True)
     parser.add_argument('-r','--random', help='Generate random MAC Address', action="store_true")
     parser.add_argument('-m','--mac', dest='new_mac',help='User set the MAC Address')
-    
-    args = parser.parse_args()
-
-    if not args.interface:
-        parser.error("[-] Please specify an interface, use --help for more info")
     
     return parser.parse_args()
        
@@ -23,39 +18,38 @@ def random_mac_address():
     return ':'.join(map(lambda x: "%02x" % x, mac))
 
 def change_mac(interface, new_mac):
-    print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
-    subprocess.call(["ifconfig", interface ,"down"])
+    print(f"[+] Changing MAC address for {interface} to {new_mac}")
+    subprocess.run(["ifconfig", interface ,"down"])
     print("[+] Setting interface down....")
-
-    subprocess.call(["ifconfig", interface , "hw" ,"ether", new_mac])
-
-    subprocess.call(["ifconfig", interface ,"up"])
+    subprocess.run(["ifconfig", interface , "hw" ,"ether", new_mac])
+    subprocess.run(["ifconfig", interface ,"up"])
     print("[+] Setting interface up....")
 
 
 def get_current_mac(interface):
-    ifconfig_result = subprocess.check_output(['ifconfig', interface])
+    ifconfig_result = subprocess.run(['ifconfig', interface], capture_output=True)
+    ifconfig_result = ifconfig_result.stdout.decode()
 
-    address_match = re.search(r"ether (\S+)", ifconfig_result.decode())
+    address_match = re.search(r"ether (\S+)", ifconfig_result)
     if address_match:
         return address_match.group(1)
     else:
         print("[-] Could not read MAC address")
 
 
-args = get_arguments()
+args = parse_args()
 
 current_mac = get_current_mac(args.interface)
-print("[+] The current MAC is: " + str(current_mac))
+print(f"[+] The current MAC is: {current_mac}")
 
 if args.random:
     new_mac = random_mac_address()
-    change_mac(args.interface , new_mac)
 else:
-    change_mac(args.interface , args.new_mac)
+    new_mac = args.new_mac
+    
+change_mac(args.interface , new_mac)
 
 if current_mac == get_current_mac(args.interface):
-    print("[+] The new mac is: " + current_mac)
+    print(f"[-] Mac adress did not get changed")
 else:
-    print("[-] Mac adress did not get changed T_T ")
+    print(f"[+] The new mac is: {get_current_mac(args.interface)}")
